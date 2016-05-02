327aa3eefad7bee45625e792a8a9c92a03fda313
Daniel Barragan <daniel.barragan@correo.icesi.edu.co>
2016-05-01 22:55:33 -0500

    add coverage

---
+++ run_tests_with_coverage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e 
+
+. ~/.virtualenvs/testproject/bin/activate
+
+PYTHONPATH=. py.test --junitxml=python_tests.xml
+PYTHONPATH=. py.test --cov-report xml --cov=../testproject
